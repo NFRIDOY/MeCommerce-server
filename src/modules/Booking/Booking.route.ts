5bdefda2658c27412a948e82d439738b0e1d344a
NF RIDOY <nfridoy@gmail.com>
2024-10-10 23:21:35 +0600

    ✅ [Update] : post Bookings + auth user

--- src/modules/Booking/Booking.route.ts
+++ src/modules/Booking/Booking.route.ts
@@ -5,9 +5,9 @@ import { auth } from '../../middlewares/auth';
 const router = express.Router();
 /** User Only */
 /** POST /api/booking 8. Create a Booking (User Only) */
-// router.post("/", auth("user"), bookingControllers.createBooking);
+router.post("/", auth("user"), bookingControllers.createBooking);
 //! testing 
-router.post("/", bookingControllers.createBooking);
+// router.post("/", bookingControllers.createBooking);
 /** GET /api/bookings/user */
 router.get("/", bookingControllers.getBookings);
 // /** DELETE /api/bookings/:id */
