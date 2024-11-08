0f1253fa847c295184a38fb50b9564caa2da10bc
NF RIDOY <nfridoy@gmail.com>
2024-11-08 06:12:26 +0600

    ✅ [Update] order model

--- src/modules/Orders/Orders.interface.ts
+++ src/modules/Orders/Orders.interface.ts
@@ -1,11 +1,12 @@
 import { Types } from "mongoose";
 import { TProduct } from "../Products/Products.interface";
-import { TUser } from "../user/user.interface";
 import { Order_status } from "./Orders.constants";
 
 export interface TOrder {
     date: Date;
-    user: Types.ObjectId | TUser;
+    // user: Types.ObjectId | TUser;
+    name: string;
+    phone: string;
     products: [Types.ObjectId];
     payableAmount: number;
     isBooked: keyof typeof Order_status;
--- src/modules/Orders/Orders.model.ts
+++ src/modules/Orders/Orders.model.ts
@@ -12,27 +12,40 @@ const orderProductSchema = new Schema({
     ...productSchemaObject,
     quantity: {
         type: Number,
-        required: [true, "Quantity is required"]
-    }
-})
+        required: [true, "Quantity is required"],
+    },
+});
 
-export const OrderProduct = model<TOrderedProduct>("OrderedProduct", orderProductSchema);
+export const OrderProduct = model<TOrderedProduct>(
+    "OrderedProduct",
+    orderProductSchema
+);
 
 const bookingSchema = new Schema<TOrder>({
     date: {
         type: Date,
         required: [true, "Date is required"],
     },
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: [true, "User ID is required"],
+    // user: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "User",
+    //     required: [true, "User ID is required"],
+    // },
+    name: {
+        type: String,
+        required: [true, "name is required"],
     },
-    products: [{
-        type: Schema.Types.ObjectId,
-        ref: "OrderProduct",
-        required: [true, "Product ID is required"],
-    }],
+    phone: {
+        type: String,
+        required: [true, "phone is required"],
+    },
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "OrderProduct",
+            required: [true, "Product ID is required"],
+        },
+    ],
     payableAmount: {
         type: Number,
         required: [true, "Payable Amount is required"],
